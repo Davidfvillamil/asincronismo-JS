e6dd686cd05cde88d5abc2160cb2d3b702c72958
David Villamil <david@Aurora.lat>
2023-03-27 18:31:57 -0500

    call back a fake API

--- Callback/challenge.js
+++ Callback/challenge.js
@@ -1,24 +1,34 @@
-const xmlhttprequest = require('xmlhttprequest')
+const XMLHttpRequest = require('xhr2')
 const API = 'https://api.escuelajs.co/api/v1'
 
-function fetchdata (url_api, callback){
-    let xhttp = new XMLHttpRequest()
-
-    xhttp.open('GET',url_api,true)
-    xhttp.onreadystatechange = function(event){
-        if(xhttp.readyState === 4) { //
-            // el estado 0 es que no se ha inicializado 
-            // el estado 1 es cuando esta Loading
-            // el estado 2 es cuaando se mando el estado sent 
-            // el estado 3 esta descargando o interactuando con la solicitud 
-            // cuando ya esta cargado 
-            if(xhttp.status === 200){ // validar que el cervidor respondio de forma correcta
-                callback(null, JSON.parse(xhttp.responseText))
-            } 
-        } else{
-            const error = new Error('error' + url_api)
-            return callback(error, null)
-        }
+function fetchData(url_api, callback) {
+  const xhttp = new XMLHttpRequest()
+  xhttp.open('GET', url_api, true)
+  xhttp.onreadystatechange = function (event) {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText))
+      } else {
+        const error = new Error('Error ' + url_api)
+        return callback(error, null)
+      }
     }
-    xhttp.send()
+  }
+  xhttp.send()
 }
+
+fetchData(`${API}/products`, function (error1, data1) {
+  if (error1) return console.error(error1)
+
+  fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+    if (error2) return console.error(error2)
+
+    fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+      if (error3) return console.error(error3)
+
+      console.log(data1[0])
+      console.log(data2.title)
+      console.log(data3.name)
+    })
+  })
+})
